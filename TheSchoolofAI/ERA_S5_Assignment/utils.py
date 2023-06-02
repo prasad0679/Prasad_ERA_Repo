b9499f605bfc99a414355f2f2ff010208b4d269f
Prasad Pradhan <prasadpradhan79@gmail.com>
2023-06-02 22:18:53 +0530

    Committing the ERA_S5.ipynb and utils.py after correction

--- TheSchoolofAI/ERA_S5_Assignment/utils.py
+++ TheSchoolofAI/ERA_S5_Assignment/utils.py
@@ -15,6 +15,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
 
 # CUDA?
 cuda = torch.cuda.is_available()
@@ -90,3 +91,14 @@ def test(model, device, test_loader,criterion):
     print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
+
+def printTrainTest_LossAcc(train_losses, train_acc, test_losses,test_acc):
+  fig, axs = plt.subplots(2,2,figsize=(15,10))
+  axs[0, 0].plot(train_losses)
+  axs[0, 0].set_title("Training Loss")
+  axs[1, 0].plot(train_acc)
+  axs[1, 0].set_title("Training Accuracy")
+  axs[0, 1].plot(test_losses)
+  axs[0, 1].set_title("Test Loss")
+  axs[1, 1].plot(test_acc)
+  axs[1, 1].set_title("Test Accuracy")
